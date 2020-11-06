eb7332f7254ee35a65368fe9ff9505732c46c8fa
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2020-11-06 14:57:47 +0530

    Added code to generate codewords
    The codewords generated using the estimated channel using pilots.
    Assumed pilots are present after every nSamples. Based on that H_LS_p we
    use it to detect the data symbols.

--- fusion_center.m
+++ fusion_center.m
@@ -16,45 +16,60 @@ clear all;
 %SNR = 20dB for all signals from SU -> FC
 %Rayleigh flat fading channel
 % BPSK modulation
-
-nSU = 10; % Number of Secondary Users
-M = 2; %BPSK modulation
-snr = 1:1:25;
-
-for i = 1:1:25 %dB
-%System Config
-N0 = 1;  % Noise power
-E_s = (10^(i/10))*N0; % Signal energy
-%snr = 10*log10(E_s/N0); %SNR of the signal
-
-% Rayleigh Fading Coefficients
-H=(randn([nSU 1])+1j*randn([nSU 1]))/sqrt(2); % Channel Matrix
-
-% Gaussian noise 
-W=sqrt(N0)*(randn([nSU 1])+randn([nSU 1])*1j)/sqrt(2);% Noise vector of CSCG noise for SU
-%W = [n0;n1;n2]; % Noise vector
-
-% Data symbols
-
-m = randi([0 1],[1,nSU])'; % Generating random message symbols
-xb = sqrt(E_s)*exp(-1i*pi*2*(m)/M); % BPSK Data symbol for all the SU
-
-X = diag(xb); % Generating the symbol matrix with the diagonal elements as the data symbols
-
-Y = X*H + W; % Output symbols at the Fusion Centre (FC)
-
-%Channel  Estimation
-H_LS = inv(X*X')*X'*Y; %MISO Least Square detection using pilots
-
-% Detection using estimated channel 
 bpsk_demod = comm.BPSKDemodulator; %Creating the bpsk demodulator object
 
+nSU = 3; % Number of Secondary Users
+M = 2; %BPSK modulation
+%snr = 1:1:25; % Use this array to generate ber vs snr plot
+
+%for i = 1:1:25 %dB
+
+%System Config
+N0 = 1;  % Noise power
+nCodeWords = 500;
+E_s =100;
+%E_s = (10^(i/10))*N0; % Signal energy
+snr = 10*log10(E_s/N0); %SNR of the signal
+nSamples = 3 ; % Number of samples across which we assume the H_coeff to be constant
+m_p = ones([nSU 1]);
+CW = zeros([nSU,nCodeWords]);
+% Rayleigh Fading Coefficients
+H=(randn([nSU nCodeWords])+1j*randn([nSU nCodeWords]))/sqrt(2); % Channel Matrix
+
+% Gaussian noise 
+W=sqrt(N0)*(randn([nSU nCodeWords])+randn([nSU nCodeWords])*1j)/sqrt(2);% Noise vector of CSCG noise for SU
+
+for i = 1: nSamples+1 :nCodeWords
+CW(:,i) = m_p;
+x_p = sqrt(E_s)*exp(-1i*pi*2*(m_p)/M); % BPSK Pilot symbol for all the SU
+
+X_p = diag(x_p); % Generating the symbol matrix with the diagonal elements as the data symbols
+
+Y_p = X_p*H(:,i) + W(:,i); % Output symbols at the Fusion Centre (FC)
+
+%Channel  Estimation
+H_LS_p = inv(X_p*X_p')*X_p'*Y_p; %MISO Least Square detection using pilots
+
+for j = i+1:i+nSamples
+
+% Data symbols
+m = randi([0 1],[1,nSU])'; % Generating random message symbols
+xb = sqrt(E_s)*exp(-1i*pi*2*(m)/M); % BPSK Data symbol for all the SU
+X_b = diag(xb); % Generating the symbol matrix with the diagonal elements as the data symbols
+Y_b = X_b*H(:,j) + W(:,j); % Output symbols at the Fusion Centre (FC)
+
+% Detection using estimated channel 
+
 % Detecting the received bits from the ZF equalisation of received vector
-xb_det = inv(diag(H_LS))*Y;
+xb_det = inv(diag(H_LS_p))*Y_b;
 m_det = bpsk_demod(xb_det);
 
 %BER
-[nErr(floor(i/1)),ratio(floor(i/1))] = biterr(m,m_det); %Generating the number of errors array and ratio array
+%[nErr(floor(i/1)),ratio(floor(i/1))] = biterr(m,m_det); %Generating the
+%number of errors array and ratio array ( include in snr for loop)
+[nErr(j),ratio(j)] = biterr(m,m_det); %Generating the number of errors array and ratio array (without for loop)
+
+CW(:,j)= m_det;
+end
 end
 
-plot(snr,nErr)
