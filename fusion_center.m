f54586b13ac14d4f23cfd361083c8c41adb55281
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2020-11-06 13:32:02 +0530

    Added ber vs snr  plot
    ZF equalisation and bpsk demod object is used for demodulation. BER vs
    SNR plot is also generated. There seems to be no errors

--- fusion_center.m
+++ fusion_center.m
@@ -15,13 +15,17 @@ clear all;
 %Number of Secondary Users (SU) : 3
 %SNR = 20dB for all signals from SU -> FC
 %Rayleigh flat fading channel
+% BPSK modulation
 
-%System Config
-nSU = 3; % Number of Secondary Users
-N0 = 1;  % Noise power
-E_s = 100; % Signal energy
-snr = 10*log10(E_s/N0); %SNR of the signal
+nSU = 10; % Number of Secondary Users
 M = 2; %BPSK modulation
+snr = 1:1:25;
+
+for i = 1:1:25 %dB
+%System Config
+N0 = 1;  % Noise power
+E_s = (10^(i/10))*N0; % Signal energy
+%snr = 10*log10(E_s/N0); %SNR of the signal
 
 % Rayleigh Fading Coefficients
 H=(randn([nSU 1])+1j*randn([nSU 1]))/sqrt(2); % Channel Matrix
@@ -32,7 +36,7 @@ W=sqrt(N0)*(randn([nSU 1])+randn([nSU 1])*1j)/sqrt(2);% Noise vector of CSCG noi
 
 % Data symbols
 
-m = randi([0 1],[1,nSU]); % Generating random message symbols
+m = randi([0 1],[1,nSU])'; % Generating random message symbols
 xb = sqrt(E_s)*exp(-1i*pi*2*(m)/M); % BPSK Data symbol for all the SU
 
 X = diag(xb); % Generating the symbol matrix with the diagonal elements as the data symbols
@@ -40,6 +44,17 @@ X = diag(xb); % Generating the symbol matrix with the diagonal elements as the d
 Y = X*H + W; % Output symbols at the Fusion Centre (FC)
 
 %Channel  Estimation
-H_mle = inv(X*X')*X'*Y;
+H_LS = inv(X*X')*X'*Y; %MISO Least Square detection using pilots
 
+% Detection using estimated channel 
+bpsk_demod = comm.BPSKDemodulator; %Creating the bpsk demodulator object
 
+% Detecting the received bits from the ZF equalisation of received vector
+xb_det = inv(diag(H_LS))*Y;
+m_det = bpsk_demod(xb_det);
+
+%BER
+[nErr(floor(i/1)),ratio(floor(i/1))] = biterr(m,m_det);
+end
+
+plot(snr,nErr)
