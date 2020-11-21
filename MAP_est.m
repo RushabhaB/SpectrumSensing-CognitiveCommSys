be863c45d48ff37b41b71d7aa0b9fe5455e39b1d
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2020-11-21 17:26:33 +0530

    Cnanged input function arg from th to fa. Returns a new argument th.

--- MAP_est.m
+++ MAP_est.m
@@ -1,13 +1,16 @@
-function CW_p = MAP_est(th,N0,E_s)
+function [th,CW_p] = MAP_est(fa,N0,E_s)
 
 gamma = E_s/N0;
-for i = 1:length(th)
+
+for i = 1:length(fa)
+
+th(i) = (qfuncinv(fa(i)/2))^2*(N0/2);
 a= sqrt(th(i))/(sqrt(N0/2)*(1+sqrt(gamma))) ;
 
 Pmd = gammainc(1,a);
 Pd=1-Pmd;
 
-Pfa=2*qfunc(sqrt(th(i)/(N0*0.5)));
+Pfa=fa(i);
 Paf = 1-Pfa;
 
 CW_p(i,:,:) = [Pd^3, Pd^2*Pmd,Pd^2*Pmd,Pd^2*Pmd,Pd*Pmd^2,Pd*Pmd^2,Pd*Pmd^2,Pmd^3;Pfa^3,Pfa*Paf^2,...
