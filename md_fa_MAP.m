d9e6c82dd03b9ee928582c554f37eb192b452ede
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2020-11-21 20:54:20 +0530

    Removed the unnecessary lines

--- md_fa_MAP.m
+++ md_fa_MAP.m
@@ -1,18 +1,9 @@
 function [p_md,p_fa] = md_fa(CW,CW_det,nSamples,nCodeWords)
 
-%a = sum(CW);
-b = CW_det;
-
-%a(a<2) = 0; % Actual PU status - Idle
-%a(a>0) = 1; % Actual PU status - Active
-a=CW;
-%b(b<2) = 0; % Estimated PU status - Idle
-%b(b>0) = 1; % Estimated PU status - Active
-
 pilots_len = length(1: nSamples+1 :nCodeWords); % Number of pilot codewords
 actual_data_len = nCodeWords - pilots_len; % Number of actual data codewords
 
-c = a-b;
+c = CW-CW_det;
 md_count = sum(c==1); % Actual is active (1) but estimated is idle (0)
 fa_count = sum(c==-1); % Actual is idle (0) but estimated is active (1)
 
