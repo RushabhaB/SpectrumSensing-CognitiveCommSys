829c1a0766fcdbe1a858a7901fd00af1c5234d94
Vinay U Pai <f20170131@pilani.bits-pilani.ac.in>
2020-11-24 19:47:10 +0530

    Added comments

--- MAP_est_Es_change.m
+++ MAP_est_Es_change.m
@@ -1,21 +1,20 @@
 function [th,CW_p] = MAP_est_Es_change(fa,N0,E_s)
-%fa = linspace(5.3201e-04,0.9887,25);
-%N0=1;
-%E_s = 100;
 
 
 for i = 1:length(E_s)
-gamma = E_s(i)/N0;
-th = (qfuncinv(fa/2))^2*(N0/2);
-a= sqrt(th)/(sqrt(N0/2)*(1+sqrt(gamma))) ;
+%Reference: "Cooperative Spectrum Sensing Using Maximum a Posteriori 
+%as a Detection Technique for Dynamic Spectrum Access Networks,"
 
-Pd = gammainc(1,a,'upper');
-Pmd=1-Pd;
+gamma = E_s(i)/N0; %SNR 
+th = (qfuncinv(fa/2))^2*(N0/2); % Threshold calculated from P_fa
+a= sqrt(th)/(sqrt(N0/2)*(1+sqrt(gamma))) ;
+Pd = gammainc(1,a,'upper'); % P_d calculated from Threshold
+Pmd=1-Pd; %P_md
 
 Pfa=fa;
 Paf = 1-Pfa;
 
 CW_p(i,:,:) = [Pd^3, Pd^2*Pmd,Pd^2*Pmd,Pd^2*Pmd,Pd*Pmd^2,Pd*Pmd^2,Pd*Pmd^2,Pmd^3;Pfa^3,Pfa*Paf^2,...
-    Pfa*Paf^2,Pfa*Paf^2,Pfa^2*Paf,Pfa^2*Paf,Pfa^2*Paf,Paf^3];
+    Pfa*Paf^2,Pfa*Paf^2,Pfa^2*Paf,Pfa^2*Paf,Pfa^2*Paf,Paf^3]; % Probabilities of each CW. Reference Table 2.
 end
 end
--- stage1_ED_Es_change.m
+++ stage1_ED_Es_change.m
@@ -1,13 +1,15 @@
 function [CW_State, CW] = stage1_ED_Es_change (nSU,nCodeword,nSamples,E_s,fa)
+
+%CW_State is the ground truth (1s mean active, 0s mean idle)
+%CW contains the codewords received by the FC from the SUs
+
 CW=[];
 
-CW_State = randi([0,1], [1,nCodeword]);
+CW_State = randi([0,1], [1,nCodeword]); % Ground truth on whether channel is active (1) or idle (0) randomly
 
-L = nSamples; % Number of sensing samples
+L = nSamples; % Coherence interval
 iter =10^5; % Number of iterations 
-%fa =0.01:0.03:1; % Probability of False Alarm
-%fa = 0.05;
-%E_p_l = E_s; % SNR in linear scale
+
 for t = 1:length(fa) % Calculating threshold for each value of false alarm
  energy_fin = []; 
  N0=1; % Noise power
@@ -23,7 +25,7 @@ end
 energy_fin = energy_fin'; % Taking transpose to arrage values in descending order
 energy_desc = sort(energy_fin,'descend')'; % Arrange values in descending order
 thresh_sim = energy_desc(ceil(fa*iter)); % Threshold obtained by simulations
-th = (qfuncinv(fa/2))^2*(N0/2);
+th = (qfuncinv(fa/2))^2*(N0/2); %Getting threshold from required local P_fa
 end
 
 for i = 1:length(CW_State)
@@ -32,26 +34,25 @@ for t = 1:length(E_s)
     s = [];  
     h = [];
     mes = randi([0 1],nSU,L); % Generating 0 and 1 with equal probability for BPSK
-    n =sqrt(N0)*randn(nSU,L); % Gaussian noise, mean 0, variance 1
+    n =sqrt(N0)*randn(nSU,L); % Gaussian noise, mean 0, variance N0
     s = (2.*(mes)-1); % BPSK modulation
     h1 = (randn(nSU,1)+1i*randn(nSU,1))./(sqrt(2)); % Generating Rayleigh channel coefficients
-    h = repmat(h1,1,L); % Slow-fading
+    h = repmat(h1,1,L); % Channel is constant over coherence interval
     if (CW_State(i) ==1)
-        y = sqrt(E_s(t)).*abs(h).*s + n; % Received signal y at the secondary user, abs(h) is the Rayleigh channel gain.
+        y = sqrt(E_s(t)).*abs(h).*s + n; % Received signal y at the SU if channel is active, abs(h) is the Rayleigh channel gain.
     else
-        y = n;
+        y = n;  % Received signal y is only noise isf channel is idle
     end
     energy = (abs(y).^2);  % Received energy 
     
-for k=1:nSU % Number of Monte Carlo Simulations
+for k=1:nSU % Number of SUs
     energy_fin_1(k,:) =sum(energy(k,:)); % Test Statistic for the energy detection 
 end
 
     detect = ((energy_fin_1/10) >= th); % Checking whether the received energy is above the threshold
-    %i_prime = sum(detect); % Count how many times out of 'iter', the received energy is above the threshold.
-    CW(i,:)=double(detect);
+    CW(i,:)=double(detect); % Convert all above threshold to 1 and below to 0
     
-    %final(final==0)=-1; % resultant matrix transmitted by SU
+
 end
 end
 end
